58e45d263ae26f20da645808f3cbf2bcc745e8c0
davidjaguilar104 <davidjaguilar104@gmail.com>
2021-11-09 16:18:48 -0600

    modularized the code

--- app.js
+++ app.js
@@ -1,26 +1,42 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
 
 
 
-// Notice the lack of parantheses around the `profileDataArr` parameter?
-const printProfileData = profileDataArr => {
+// // Notice the lack of parantheses around the `profileDataArr` parameter?
+// const printProfileData = profileDataArr => {
 
-    // This...
-    for(let i = 0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
+//     // This...
+//     for(let i = 0; i < profileDataArr.length; i++) {
+//         console.log(profileDataArr[i]);
+//     }
 
-    console.log('================');
+//     console.log('================');
 
-    // Is the same as this...
-    profileDataArr.forEach((profileItem) => {
-        console.log(profileItem);
-    });
+//     // Is the same as this...
+//     profileDataArr.forEach((profileItem) => {
+//         console.log(profileItem);
+//     });
 
-    console.log('================');
+//     console.log('================');
 
-    // Using arrow functions it can be made even cleaner like this
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+//     // Using arrow functions it can be made even cleaner like this
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
 
-printProfileData(profileDataArgs);
+// printProfileData(profileDataArgs);
+
+// CODE ABOVE IS JUST FOR REFERENCE TO WHAT WAS LEARNED IN LESSON 1 
+
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if(err) throw new Error(err);
+
+    console.log('Portfolio complete! Check out index.html to see the output!')
+});
