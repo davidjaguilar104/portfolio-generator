3e53725131690629b63adcc873d2bde93d4c8ebd
davidjaguilar104 <davidjaguilar104@gmail.com>
2021-11-11 14:56:21 -0600

    added .filter/.map/.join

--- app.js
+++ app.js
@@ -30,7 +30,8 @@
 
 
 
-
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 const inquirer = require('inquirer');
 
 
@@ -170,12 +171,17 @@ const promptProject = portfolioData => {
 promptUser()
 .then(promptProject)
 .then(portfolioData => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw new Error(err);
+
+      console.log('Page created! Check out index.html in this directory to see it!');
+    });
 });
 
 
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
+
 
 // const pageHTML = generatePage(name, github);
 
